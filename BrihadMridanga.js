f2fbccfdaa6518801a40d38b80620ccca9eea6d6
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-01-21 15:26:08 +0530

    ok

---
+++ BrihadMridanga.js
@@ -0,0 +1,258 @@
+// ==UserScript==
+// @name         Brihat Mridanga
+// @namespace    https://web.whatsapp.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://web.whatsapp.com/
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+const PROFILE_HEADER_CLASS = "YtmXM"
+const SEND_BUTTON = "p2rjqpw5"
+const INVALID_NUM = "_2Nr6U"
+
+function sleep(n) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, n);
+  });
+}
+
+async function waitForClass(c){
+    while(true){
+        var e = document.getElementsByClassName(c)
+        if(e.length==0){
+            await sleep(2500)
+            continue
+        }
+        return
+    }
+}
+
+async function waitToOpen(w,c){
+    while(true){
+        var e = w.window.document.getElementsByClassName(c)
+        if(e.length==0){
+            await sleep(2500)
+            continue
+        }
+        return
+    }
+}
+
+async function waitForSendOrFail(w,c){
+    var retry1 = 0
+    var retry2 = 0
+    var sl = 500
+    while(true){
+        var e = w.window.document.getElementsByClassName(c)
+        // console.log(`Found ${e.length} buttons with classname ${c}`)
+        if(e.length==0){
+            var e2 = w.window.document.getElementsByClassName(INVALID_NUM)
+            if(e2.length==0){
+                await sleep(1000)
+                retry1++
+                if(retry1==30){
+                    return 'Timed-Out'
+                }
+                continue
+            }else if(e2[0].textContent=="Phone number shared via url is invalid."){
+                return 'Invalid Number'
+            }else{
+                if(retry2==60){
+                    return 'Unknown issue encountered!'
+                }
+                retry2++
+                await sleep(sl)
+                sl+=500
+                continue
+            }
+        }else{
+            return 'OK'
+        }
+    }
+}
+
+function isURL(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function parseLinks(rawText){
+    console.log("parsing raw text")
+    const links = rawText.split("\n")
+    console.log(`${links.length} lines of raw text found`)
+    var allLinks = links.filter((l)=>{return isURL(l)}).map((l)=>{
+
+        var obj = {}
+        obj.url = new URL(l);
+        obj.phone = obj.url.searchParams.get('phone');
+        obj.name = decodeURI(obj.url.searchParams.get('name'))
+        obj.text = decodeURI(obj.url.searchParams.get('text'))
+        obj.link=l
+
+        return obj
+    })
+    console.log(`${allLinks.length} links created`)
+    return allLinks
+}
+
+function createLinksInput(){
+    var linksInput = document.createElement('textarea')
+    linksInput.id="raw"
+    var body = document.body
+    body.appendChild(linksInput)
+
+    linksInput.addEventListener("input",(e)=>{
+        console.log("Changes found in input")
+        var links = parseLinks(e.target.value)
+        console.log(`${links.length} links found`)
+
+        var tb = document.getElementById("list")
+        console.log(`getting list element ${tb}`)
+        if(tb){
+            tb.outerHTML = ""
+        }
+        tb = document.createElement('table')
+        tb.id="list"
+
+        window.document.title=`${links.length} links found`
+
+        console.log(`${links.length} links found`)
+
+        links.map((link, i)=>{
+
+            var r = document.createElement("tr")
+
+            var nc = document.createElement("td")
+            nc.textContent = link.name
+            nc.title= link.text
+            nc.style="padding:8px"
+            r.appendChild(nc)
+
+            var pc = document.createElement("td")
+            pc.textContent = link.phone
+            pc.style="padding:8px"
+            r.appendChild(pc)
+
+            var sc = document.createElement("td")
+            sc.textContent = "Pending"
+            sc.style="padding:8px"
+            sc.id=`${link.phone}-${i}`
+            r.appendChild(sc)
+
+            var timec = document.createElement("td")
+            timec.textContent = ""
+            timec.style="padding:8px"
+            timec.id=`time-${link.phone}-${i}`
+            r.appendChild(timec)
+
+            var ob = document.createElement("button")
+            ob.textContent = "👁"
+            ob.addEventListener("click",()=>{
+                window.open(link.link)
+            })
+            r.appendChild(ob)
+
+            tb.appendChild(r)
+        })
+        body.appendChild(tb)
+    })
+
+    console.log("Added input event listener for textarea")
+
+    return linksInput
+}
+
+function createSendButton(){
+    var sendButton = document.createElement('button')
+    sendButton.addEventListener("click",send)
+    sendButton.innerHTML="Send All"
+    sendButton.id="sendAll"
+    sendButton.style="color:black;background:white;padding:8px"
+    document.body.appendChild(sendButton)
+    return sendButton
+}
+
+async function send(e){
+
+    e.target.remove()
+
+    var ws = []
+    var tabs = 10
+    for(var k=0; k<tabs; k++){
+        ws.push(window.open(""))
+    }
+
+    var data = parseLinks(document.getElementById("raw").value)
+    var w
+    var elapsedSeconds = 0
+    for(var i=0; i<data.length;i++){
+        window.document.title=`Sending ${i+1} of ${data.length}`
+        if(elapsedSeconds>=10000000000000){
+            w.close()
+            await sleep(500)
+            w = window.open("")
+            await sleep(500)
+            w.blur()
+            window.focus()
+        }
+        w = ws[i%tabs]
+        w.window.open(data[i].link,"_self")
+        await sleep(100)
+        var current = document.getElementById(`${data[i].phone}-${i}`)
+        var time = document.getElementById(`time-${data[i].phone}-${i}`)
+        current.textContent="Sending..."
+        // current.scrollIntoView()
+        var t1 = new Date().getTime()
+        await waitToOpen(w,PROFILE_HEADER_CLASS)
+       // console.log("starting to wait", new Date())
+        var msg = await waitForSendOrFail(w,SEND_BUTTON)
+        //console.log("wait is over", new Date())
+        //console.log("msg:",msg)
+        switch(msg){
+            case 'OK':
+                //console.log("waiting to send", new Date())
+                await sleep(150)
+                w.window.document.getElementsByClassName(SEND_BUTTON)[0].click()
+                //console.log("sent", new Date())
+                await sleep(300)
+                current.textContent="Sent"
+                break
+            default:
+                current.textContent = msg
+        }
+        var t2 = new Date().getTime()
+        elapsedSeconds = Math.floor((t2-t1)/1000)
+        time.textContent = `${elapsedSeconds}s`
+    }
+    window.document.title=`Sent all!`
+}
+
+(async function() {
+    'use strict';
+
+    console.log("waiting for whatsapp to load")
+    await waitForClass(PROFILE_HEADER_CLASS)
+
+    console.log("clearing contents")
+    var body = document.body
+    while(document.body.childElementCount>0){
+        body.removeChild(body.children[0])
+    }
+
+    body.style="overflow:scroll"
+
+    createLinksInput()
+    createSendButton()
+})();
